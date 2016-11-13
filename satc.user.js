be561cbbce30a05ab6f7800101eb86aa10e5413a
BJIAST <bjiast.01@gmail.com>
2016-11-13 17:34:06 +0200

    syntax fix

--- satc.user.js
+++ satc.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         AutoTrade Confirm
-// @namespace    https://github.com/BJIAST/SATC/raw/master/satc.user.js
-// @version      1.0
+// @namespace    https://github.com/BJIAST/SATC/
+// @version      1.1
 // @description  try to take over the world!
 // @author       BJIAST
 // @match        https://steamcommunity.com/tradeoffer/*
@@ -11,27 +11,29 @@
 
 (function() {
 	'use strict';
-	(function () {
-		var soundAccept = new Audio('https://raw.githubusercontent.com/BJIAST/SATC/master/sounds/done.mp3');
-		var site = location.href,
-		steamsite = location.href.split("trade/");
-		if (document.referrer == "http://cs.money/"){
-			if (confirm('Принять этот трейд?')){
-				acceptSteamTrade();
-			}
-		}else if (jQuery("#your_slot_0 .slot_inner").html() == ''){
+
+	var soundAccept = new Audio('https://raw.githubusercontent.com/BJIAST/SATC/master/sounds/done.mp3'),
+	site = location.href,
+	steamsite = location.href.split("trade/");
+
+	// accept conditions
+	if (document.referrer == "http://cs.money/"){
+		if (confirm('Принять этот трейд?')){
 			acceptSteamTrade();
-		}else if(site == steamsite[0] + "trade/" + steamsite[1]){
-			soundAccept.play();
-			setTimeout(function(){
-				window.close();
-			}, 3000);
-		}else{
-			console.log("Что то не так!");
 		}
-	}());
+	}else if (jQuery("#your_slot_0 .slot_inner").html() == ''){
+		acceptSteamTrade();
+	}else if(site == steamsite[0] + "trade/" + steamsite[1]){
+		soundAccept.play();
+		setTimeout(function(){
+			window.close();
+		}, 3000);
+	}else{
+		console.log("Что то не так!");
+	}
+	
+	// accept function
 	function acceptSteamTrade(){
-		var offerWindow = location.href;
 		setInterval(function(){
 			ToggleReady(true);
 			jQuery(".newmodal_buttons .btn_green_white_innerfade span").click();
