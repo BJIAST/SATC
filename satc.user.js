f53e26bd4d9f8ae0807722508ab7628d07569a45
BJIAST <bjiast.01@gmail.com>
2016-12-08 08:01:39 +0200

    start new v

--- satc.user.js
+++ satc.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         AutoTrade Confirm
 // @namespace    https://github.com/BJIAST/SATC/
-// @version      1.532
+// @version      2
 // @description  try to take over the world!
 // @author       BJIAST
 // @match        https://steamcommunity.com/tradeoffer/*
@@ -11,142 +11,96 @@
 // ==/UserScript==
 
 (function() {
-	'use strict';
-	include("https://cdnjs.cloudflare.com/ajax/libs/jquery-cookie/1.4.1/jquery.cookie.min.js");
+    'use strict';
+    include("https://cdnjs.cloudflare.com/ajax/libs/jquery-cookie/1.4.1/jquery.cookie.min.js");
 
-	var soundAccept = new Audio('https://raw.githubusercontent.com/BJIAST/SATC/master/sounds/done.mp3'),
-	site = location.href,
-	steamsite = location.href.split("trade/"),
-	websendoff = location.href.split("tradeoffer/new"),
-	x,
-	csmanyaccept = location.href.split("tradeoffer/");
-
-	setTimeout(function(){
-		if ($.cookie("selection") == "off"){
-			$("#favorite").css({"display":"none"});         
-			$("#sorting").css({"display":"none"});              
-			$("#sort_inputs").css({"display":"none"});          
-			$("#sticker_sort").css({"display":"none"});
-			$("#backBtns").css({"display":"block"});
-			$("#delBackBtns").css({"display":"none"});
-			$("#sticker_count").css({"display":"none"});
-			$("#sticker_names").css({"display":"none"});
-		}
-	},2000);
-	(function () {
-		   if (site == "http://cs.money/"){
-
-			$(".trade_lg").append("<div class='btn btn-info' id='delBackBtns'>Убрать кнопки</div>");
-			$(".trade_lg").append("<div class='btn btn-info' id='backBtns'>Вернуть кнопки</div>");
-			$("#backBtns").css({"display":"none"});
-			$("#delBackBtns").css({
-				"margin": "20px 35px auto"
-			});
-			$("#backBtns").css({
-				"margin": "20px 35px auto"
-			});
-			$("#delBackBtns")['on']("click", function(){
-				$.cookie('selection', "off");
-				$("#backBtns").css({"display":"block"});
-				$("#delBackBtns").css({"display":"none"});
-				$("#favorite").css({"display":"none"});         
-				$("#sorting").css({"display":"none"});          
-				$("#sort_inputs").css({"display":"none"});          
-				$("#sticker_sort").css({"display":"none"});
-				$("#sticker_count").css({"display":"none"});
-				$("#sticker_names").css({"display":"none"});
-
-			});
-			$("#backBtns")['on']("click", function(){
-				$.cookie('selection', null);
-				$("#backBtns").css({"display":"none"});
-				$("#delBackBtns").css({"display":"block"});
-				$("#favorite").css({"display":"block"});            
-				$("#sorting").css({"display":"block"});         
-				$("#sort_inputs").css({"display":"block"});         
-				$("#sticker_sort").css({"display":"block"});
-				$("#sticker_count").css({"display":"inline-block"});
-				$("#sticker_names").css({"display":"inline-block"});
-			});
-		}
-	 }());
-
-
-// chrome messages
-function chromemes(mesbody){
-	var currentPermission;
-	Notification.requestPermission( function(result) { currentPermission = result } );
-	mailNotification = new Notification("SATC", {
-		body : mesbody,
-		icon : "https://pp.vk.me/c7004/v7004148/23616/XwoiYEex0CQ.jpg"
-	});
-}
+    var soundAccept = new Audio('https://raw.githubusercontent.com/BJIAST/SATC/master/sounds/done.mp3'),
+    web = location.href,
+    fromWeb = document.referrer,
+    steamsite = location.href.split("/receipt"),
+    sendoffer = location.href.split("/new/"),
+    tradeoffer = location.href.split("tradeoffer/"),
+    fromCut = document.referrer.split("tradeoffer/");
 
 // accept conditions
-if(site != "http://cs.money/"){
-	if(document.referrer == "http://cs.money/" && site == websendoff[0] + "tradeoffer/new" + websendoff[1] && site != "http://cs.money/#"){
+if (web == tradeoffer[0] + "tradeoffer/" + tradeoffer[1] && web != sendoffer[0] + "/new/" + sendoffer[1] && !jQuery("#your_slot_0 .slot_inner").html()){
+    offerAccept();
+    // console.log("Я это приму");
 
-		console.log("Do nothing!");
-
-	}else if (document.referrer == "http://cs.money/"){
-
-		if(jQuery('.error_page_content h3').html() == "О не-е-е-е-е-е-е-т!"){
-			window.close();
-			chromemes("Оффер не действителен!");
-		}else if(site == csmanyaccept[0] + "tradeoffer/" + csmanyaccept[1] + "/"){
-			if (confirm('Принять этот трейд?')){
-				acceptSteamTrade();
-                // testclosing
-                // setTimeout(function(){
-                // 	if (x === "true"){
-                // 		window.close();
-                // 	}
-                // },4000);
-            }
-        }
+}else if (web == tradeoffer[0] + "tradeoffer/" + tradeoffer[1] && fromWeb == "https://opskins.com/?loc=sell" || web == tradeoffer[0] + "tradeoffer/" + tradeoffer[1] && fromWeb == "http://cs.money/"){
+    if (jQuery('.error_page_content h3').html() == "О не-е-е-е-е-е-е-т!"){
+        setTimeout(function(){
+            window.close();
+        }, 300);
+        chromemes("Оффер не действителен!");
     }
-    else if (site == websendoff[0] + "tradeoffer/new" + websendoff[1]){
-    	console.log("Do nothing!");
-    }
-    else if (jQuery("#your_slot_0 .slot_inner").html() == ''){
-    	acceptSteamTrade();
-    }
-    else if(site == steamsite[0] + "trade/" + steamsite[1]){
-    	soundAccept.play();
-    	setTimeout(function(){
-    		window.close();
-    	}, 3000);
-    }
-    else if(document.referrer == "https://opskins.com/?loc=sell"){
-    	acceptSteamTrade();
-    }else{
-    	console.log("Что то не так!");
-    }
+}else{
+    console.log("Я это не приму или сайт не подходит!");
+}
+if(web == steamsite[0] + "/receipt" && fromWeb == fromCut[0] + "tradeoffer/" + fromCut[1]){
+    soundAccept.play();
+    setTimeout(function(){
+        window.close();
+    }, 3000);
+    chromemes("Скин забрал!");
+    // console.log("Я это закрою");
 }
 
 
-// accept function
-function acceptSteamTrade(){
-	setInterval(function(){
-		if (jQuery('.newmodal_content div').html() != "Для завершения обмена подтвердите его на странице подтверждений в мобильном приложении Steam."){
-			jQuery(".newmodal").remove();
-			ToggleReady(true);
-			jQuery(".newmodal_buttons .btn_green_white_innerfade span").click();
-			ConfirmTradeOffer();
-		
-		}else{
-            soundAccept.play();
-            window.open('', '_self', ''); //bug fix
-            window.close();
-            chromemes("Подтверди оффер в телефоне!");
-			return x = true;
-        }
-    },3000);
-};
 
+
+// Main function
+function offerAccept(){
+    setInterval(function(){
+      if (jQuery('.newmodal_content>div').html() == "Для завершения обмена подтвердите его на странице подтверждений в мобильном приложении Steam."){
+        soundAccept.play();
+        window.close();
+    }else{
+        jQuery(".newmodal").remove();
+        ToggleReady(true);
+        if(jQuery(".newmodal_buttons .btn_green_white_innerfade span")){
+            jQuery(".newmodal_buttons .btn_green_white_innerfade span").click();                
+        }
+        ConfirmTradeOffer();
+    }
+},3000);
+}
+// func to include another js
 function include(url) {
-	var script = document.createElement('script');
-	script.src = url;
-	document.getElementsByTagName('head')[0].appendChild(script);
+    var script = document.createElement('script');
+    script.src = url;
+    document.getElementsByTagName('head')[0].appendChild(script);
+}
+// chrome messages
+function chromemes(mesbody){
+    var currentPermission;
+    Notification.requestPermission( function(result) { currentPermission = result } );
+    mailNotification = new Notification("Steam Auto Trade Confirm", {
+        body : mesbody,
+        icon : "https://pp.vk.me/c7004/v7004148/23616/XwoiYEex0CQ.jpg"
+    });
+}
+// inside div with message
+function showlogs(logmes){
+    $(".logmessage").remove();
+    $("body").append("<div class='fa fa-check-circle logmessage'><span>" + " " + logmes + "</span></div>");
+    $(".logmessage").css({
+        "position" : "fixed",
+        "bottom" : "20px",
+        "right" : "10px",
+        "font-size" : "16px",
+        "padding": "10px 29px 8px 40px",
+        "border": "1px solid #026194",
+        "border-radius": "10px",
+        "-moz-border-radius": "10px",
+        "-webkit-border-radius": "10px",
+        "box-shadow": "2px 2px 3px #bbb",
+        "-moz-box-shadow": "2px 2px 3px #bbb",
+        "-webkit-box-shadow": "2px 2px 3px #bbb",
+        "background": "#fff",
+        "text-align":"justify",
+        "color": "#000"
+    });
+    $(".logmessage").fadeIn(300).delay(4500).fadeToggle(300);
 }
 })();
